c874e3f750d11acfbf9ef9e057e715da6f606669
FacuRob <roblesfacundo7@gmail.com>
2025-03-19 23:38:54 -0300

    JWT, Validaciones en Back-end, modificaciones y agregar formulario Front-end

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,39 +1,36 @@
-import { Router } from 'express'
-import { body } from 'express-validator'
-import { createAccount, login } from './handlers'
-import { handleInputErrors } from './middleware/validation'
+import { Router } from "express";
+import { body } from "express-validator";
+import { createAccount, getUser, login } from "./handlers";
+import { handleInputErrors } from "./middleware/validation";
+import { authenticate } from "./middleware/auth";
 
-
-const router = Router()
+const router = Router();
 
 /** Authenticacion y Registro */
-router.post('/auth/register', 
-    body('handle')
-        .notEmpty()
-        .withMessage('El Handle no puede ir vacio'),
-    body('name')
-        .notEmpty()
-        .withMessage('El Nombre no puede ir vacio'),
-    body('email')
-        .isEmail() //valida el formato de un Email
-        .withMessage('E-mail no válido'),
-    body('password')
-        .isLength({min: 8})
-        .withMessage('La Contraseña debe tener un Minimo de 8 Caracteres'),
-    handleInputErrors,
-    createAccount
-)
+router.post(
+  "/auth/register",
+  body("handle").notEmpty().withMessage("El Handle no puede ir vacio"),
+  body("name").notEmpty().withMessage("El Nombre no puede ir vacio"),
+  body("email")
+    .isEmail() //valida el formato de un Email
+    .withMessage("E-mail no válido"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("La Contraseña debe tener un Minimo de 8 Caracteres"),
+  handleInputErrors,
+  createAccount
+);
 
-router.post('/auth/login',
-    body('email')
-        .isEmail() //valida el formato de un Email
-        .withMessage('E-mail no válido'),
-    body('password')
-        .notEmpty()
-        .withMessage('La Contraseña es obligatoria'),
-    handleInputErrors,
-    login 
-)
+router.post(
+  "/auth/login",
+  body("email")
+    .isEmail() //valida el formato de un Email
+    .withMessage("E-mail no válido"),
+  body("password").notEmpty().withMessage("La Contraseña es obligatoria"),
+  handleInputErrors,
+  login
+);
 
+router.get("/user", authenticate, getUser);
 
-export default router
+export default router;
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,18 +1,28 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LoginView from './views/LoginView'
-import RegisterView from './views/RegisterView'
-import AuthLayouts from './layouts/AuthLayouts'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginView from "./views/LoginView";
+import RegisterView from "./views/RegisterView";
+import AuthLayouts from "./layouts/AuthLayouts";
+import AppLayout from "./layouts/AppLayout";
+import LinkTreeView from "./views/LinkTreeView";
+import ProfileView from "./views/ProfileView";
 
 export default function Router() {
-    
-     return(
-        <BrowserRouter>
-            <Routes>
-                <Route element={<AuthLayouts />}>
-                    <Route path='/auth/login' element={<LoginView />} />
-                    <Route path='/auth/register' element={<RegisterView />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthLayouts />}>
+          <Route path="/auth/login" element={<LoginView />} />
+          <Route path="/auth/register" element={<RegisterView />} />
+        </Route>
+
+        <Route path="/admin" element={<AppLayout />}>
+          {" "}
+          //Ruta padre
+          <Route index={true} element={<LinkTreeView />} />
+          <Route path="profile" element={<ProfileView />} /> //Ruta Hijo -
+          /admin/profile
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
